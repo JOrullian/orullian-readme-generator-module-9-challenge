83fcc2792cde84f3219aa726bde07cadce2cd471
JOrullian <jedediahorullian@gmail.com>
2024-08-02 17:21:34 -0500

    renderLicenseBadge and renderLicenseLink both output correctly and generateMarkdown places the license string in the README

--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,59 +1,61 @@
-//Function that returns a license badge based on which license is passed in. If there is no license, return an empty string.
+// Function that returns a license badge based on which license is passed in. If there is no license, return an empty string.
 function renderLicenseBadge(license) {
   let licenseAlt = '';
 
-  if (license === 'Apache 2.0') {
+  console.log(`Recieved license: ${license}`);
+
+  if (license[0] === 'Apache 2.0') {
     // Convert license input to the correct format for the badge
     licenseAlt = 'Apache_2.0';
-  } else if (license === 'BSD 3-Clause') {
+  } else if (license[0] === 'BSD 3-Clause') {
     licenseAlt = 'BSD_3--Clause';
-  } else if (license === 'Eclipse Public License 1.0') {
+  } else if (license[0] === 'Eclipse Public License 1.0') {
     licenseAlt = 'EPL_1.0';
-  } else if (license === 'GNU GPL v3') {
+  } else if (license[0] === 'GNU GPL v3') {
     licenseAlt = 'GPLv3';
-  } else if (license === 'IBM Public License 1.0') {
+  } else if (license[0] === 'IBM Public License 1.0') {
     licenseAlt = 'IPL_1.0';
-  } else if (license === 'MIT') {
+  } else if (license[0] === 'MIT') {
     licenseAlt = 'MIT';
-  } else if (license === 'Mozilla Public License 2.0') {
+  } else if (license[0] === 'Mozilla Public License 2.0') {
     licenseAlt = 'MPL_2.0';
-  } else if (license === 'Attribution License (BY)') {
+  } else if (license[0] === 'Attribution License (BY)') {
     licenseAlt = 'ODC_BY';
   }
 
-  const licenseBadge = `![License: ${license}](https://img.shields.io/badge/License-${licenseAlt}-green.svg)`;
+  console.log(`Assigned licenseAlt: ${licenseAlt}`);
 
-  if (!license) {
-    return '';
+  let licenseBadge = '';
+
+  if (license) {
+    licenseBadge = `![License: ${license}](https://img.shields.io/badge/License-${licenseAlt}-green.svg)`;
   }
 
   return licenseBadge;
 }
 
+// console.log(renderLicenseBadge('Apache 2.0'));
+
 // Function that returns the license link. If there is no license, return an empty string.
 function renderLicenseLink(license) {
   let licenseLink = '';
   // Convert license input to correct format for the link
-  if (license === 'Apache 2.0') {
+  if (license[0] === 'Apache 2.0') {
     licenseLink = '(https://opensource.org/licenses/Apache-2.0)';
-  } else if (license === 'BSD 3-Clause') {
+  } else if (license[0] === 'BSD 3-Clause') {
     licenseLink = '(https://opensource.org/licenses/BSD-3-Clause)';
-  } else if (license === 'Eclipse Public License 1.0') {
+  } else if (license[0] === 'Eclipse Public License 1.0') {
     licenseLink = '(https://opensource.org/licenses/EPL-1.0)';
-  } else if (license === 'GNU GPL v3') {
+  } else if (license[0] === 'GNU GPL v3') {
     licenseLink = '(https://www.gnu.org/licenses/gpl-3.0)';
-  } else if (license === 'IBM Public License 1.0') {
+  } else if (license[0] === 'IBM Public License 1.0') {
     licenseLink = '(https://opensource.org/licenses/IPL-1.0)';
-  } else if (license === 'MIT') {
+  } else if (license[0] === 'MIT') {
     licenseLink = '(https://opensource.org/licenses/MIT)';
-  } else if (license === 'Mozilla Public License 2.0') {
+  } else if (license[0] === 'Mozilla Public License 2.0') {
     licenseLink = '(https://opensource.org/licenses/MPL-2.0)';
-  } else if (license === 'Attribution License (BY)') {
+  } else if (license[0] === 'Attribution License (BY)') {
     licenseLink = '(https://opendatacommons.org/licenses/by/)';
-  }
-
-  if (!licenseLink) {
-    return '';
   }
 
   return licenseLink;
@@ -64,7 +66,9 @@ function generateMarkdown(license) {
   const licenseBadge = renderLicenseBadge(license);
   const licenseLink = renderLicenseLink(license);
 
-  return `${licenseBadge}${licenseLink}`;
+  console.log(licenseBadge + licenseLink)
+
+  return `[${licenseBadge}]${licenseLink}`;
 }
 
 // Export file
